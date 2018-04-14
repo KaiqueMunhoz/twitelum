8848b60b16991054ae7b0b9ad6f3bb940621953f
KaiqueMunhoz <munhoz13km@gmail.com>
2018-04-14 01:46:02 -0300

    Added the custom component 'PrivateRoute'

--- twitelum-front/src/routes.js
+++ twitelum-front/src/routes.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Redirect, Route, Switch} from 'react-router-dom';
 
 //Pages
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 
+function isAuthenticated() { 
+  return localStorage.getItem('TOKEN'); 
+}
+
+class PrivateRoute extends Component {
+
+  render(){
+
+
+    const Component = this.props.component;
+    const props = this.props;
+
+    if(isAuthenticated){
+      return (
+        <Route render={() => <Component {...props} />}/>
+      );
+    }else {
+      return(
+        <Redirect to="/login" />
+      );
+    }
+  };
+}
+
 const Routes = () =>  {
 
-  function isAuthenticated() { 
-    return localStorage.getItem('TOKEN'); 
-  }
+
 
   return(
     <Switch>
-      <Route path='/' exact component={Home}/>
+      <PrivateRoute path='/' exact component={Home}/>
       <Route path='/login' component={LoginPage}/>
     </Switch>
   );
