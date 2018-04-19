c9aad816dae4cf10d8f7e9668a2b01c415ec602c
KaiqueMunhoz <munhoz13km@gmail.com>
2018-04-19 18:43:35 -0300

    Added the 'divs' of modal

--- twitelum-front/src/components/Modal/index.js
+++ twitelum-front/src/components/Modal/index.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Modal extends Component {
+
+    static propTypes = {
+        isOpen = PropTypes.bool,
+        closeModal = PropTypes.func.isRequired
+    }
+
     render(){
         return (
-            <div></div>
+            <div className={`modal ${this.props.isOpen ? 'modal--active' : ''}`} onClick={this.props.closeModal}>
+            {
+                this.props.isOpen &&
+                <div className={'modal__wrap'}>
+                    {this.props.children}
+                </div>
+            }
+
+            </div>
         );
     }
 }
